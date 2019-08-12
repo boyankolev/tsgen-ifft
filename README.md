d7ef0ac3d66faf8f52abe29fac8f428830032e4a
boyankolev <bkolev@gmail.com>
2019-08-12 16:05:10 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# tsgen-ifft
-Frequency-based time series generator
+# Frequency-based time series generator
+
+This tool generates random time series with desired frequency characteristics. This is done by first generating the frequency spectra of the time series and then applying an inverse Fourier transform plus some additional white noise to generate the series themselves. The spectrum generation assigns high amplitudes to `freqnum` subsequent frequency components, starting  from  the `freqstart`<sup>th</sup> one,  and  low  ones  for the rest. Randomness is applied to the phase shifts of all frequencies, so that the generated set of series can enjoy a good variety.
+
+## Usage
+<pre>
+
+$SPARK_HOME/bin/spark-submit tsgen_ifft.py [-h] -fn FREQNUM [-fs FREQSTART] -tl TSLEN -tn TSNUM -o OUTFILE
+
+Arguments:
+  -h, --help            show this help message and exit
+  -fn FREQNUM           number of significant frequencies
+  -fs FREQSTART         first frequency index [default=0]
+  -tl TSLEN             length of time series
+  -tn TSNUM             number of time series
+  -o OUTFILE            output file
+</pre>
+
+### Prerequisites
+
+- Python, version 2.7 or later
+- numpy, version 1.16 or later
+- Spark, version 2.2.0 or later
+
---
+++ tsgen-ifft.py
@@ -0,0 +1,51 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-fn", "--freqnum", help="number of significant frequencies", type=int, required=True)
+parser.add_argument("-fs", "--freqstart", help="first frequency index", default=0)
+parser.add_argument("-tl", "--tslen", help="length of time series", type=int, required=True)
+parser.add_argument("-tn", "--tsnum", help="number of time series", type=int, required=True)
+parser.add_argument("-o", "--outfile", help="output file", required=True)
+args = parser.parse_args()
+
+k = args.freqnum
+startk = args.freqstart
+tslen = args.tslen
+tsnum = args.tsnum
+outfile = args.outfile
+
+
+import random
+import numpy as np
+
+def genTS(k, startk, tslen, s):
+  random.seed(s)
+  
+  freecoefs = [random.random() for i in range(tslen/2 - 1 - k)]
+  w = [1 for i in range(k)]
+  bw = (tslen*tslen/2 - sum(freecoefs)) / sum(w)
+  
+  coefs = np.sqrt(np.concatenate( (freecoefs[:startk], bw*np.array(w), freecoefs[startk:]) ))
+  coefs_all = np.concatenate(([0.], coefs, [(random.random()-0.5) * 2], coefs[::-1]))
+  
+  angles = np.array([(random.random()-0.5) * 2*np.pi for i in range(tslen/2 - 1)])
+  angles_all = np.concatenate(([0.], angles, [0.], -angles[::-1]))
+  
+  tsft = [r * np.exp(1j * fi) for r, fi in zip(coefs_all, angles_all)]
+  ts = np.fft.ifft(tsft).real + np.array([random.gauss(0, 1)/4 for i in range(tslen)])
+  return ts
+
+
+from pyspark import SparkConf, SparkContext
+from pyspark.mllib.random import RandomRDDs
+
+conf = SparkConf()
+sc = SparkContext(conf = conf)
+
+RandomRDDs.uniformRDD(sc, tsnum).zipWithIndex() \
+  .map( lambda (r, i): (i, genTS(k, startk, tslen, int(r*1000) + (i+1)*1000)) ) \
+  .map( lambda (i, ts): str(i) + ',' + ','.join(["%.5f" % x for x in ts]) ) \
+  .saveAsTextFile(outfile)
+
+# Example: generate of F5_2 dataset of 1000 time series of length 256:
+# python gendata_ifft.py -fn 5 -fs 2 -tl 256 -tn 1000 -o Random_F5_2.csv
